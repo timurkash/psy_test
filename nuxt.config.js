30ef8d67a909426d91f36f823a0e39752b05e348
timurkash <timurkash@yandex.ru>
2019-12-14 13:51:31 +0300

    Initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'Find-Psy',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
